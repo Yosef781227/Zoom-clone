6a3ad2394a28dfbcf1cc9aed9360da008a8f7011
Yosef781227 <yosefnakachew101@gmail.com>
2022-06-09 13:56:44 +0300

    frist zoom clone commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
 import 'package:zoom_clone/utils/colors.dart';
 
-void main() {
+void main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -24,6 +28,7 @@ class MyApp extends StatelessWidget {
         routes:
         {
           '/login' : (context) => const LoginScreen(),
+          '/home-screen'  :(context) => const HomeScreen(),
         }
     );
 
